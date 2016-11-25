0d3c89cf400a394856559ccd9df8623c0104ce7b
vishnu45 <vishnu4v5@gmail.com>
2016-11-25 00:00:39 -0500

    ADD: App config

--- sfmovies/src/main/java/com/vish/sfmovies/App.java
+++ sfmovies/src/main/java/com/vish/sfmovies/App.java
@@ -1,9 +1,21 @@
 package com.vish.sfmovies;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+// used instead of bean annotations in AppConfig
+@Component
+// to work without having singleton
+@Scope("prototype")
+public class App {
+	
+	// We cannot use component in this case where there is a parameterized constructor, since the default constructor will no longer exist
+//	public HelloSpring(String name) {
+//		
+//	}
+	
+	public void sayHello() {
+		System.out.println("Hello Spring");
+	}
+	
 }
